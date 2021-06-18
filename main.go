3486a1b8f27e4ada5a7542aa30afd02bf596fc1d
Prem Saraswat <prmsrswt@gmail.com>
2021-06-19 02:31:52 +0530

    Add total requests counter

--- main.go
+++ main.go
@@ -7,14 +7,27 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 const (
 	addr = "0.0.0.0:8080"
+	name = "demo"
+)
+
+var (
+	requestsTotal = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name:      "http_requests_total",
+		Namespace: name,
+		Help:      "Total number of incoming HTTP requests.",
+	}, []string{"handler", "code"})
 )
 
 func sayPong(w http.ResponseWriter, r *http.Request) {
+	requestsTotal.WithLabelValues("ping", "200").Inc()
+
 	delay := rand.Float64() * 5 * 1000
 	time.Sleep(time.Millisecond * time.Duration(delay))
 	fmt.Fprintf(w, "pong after %ds!", int(delay/1000))
@@ -25,9 +38,13 @@ func russianRoulette(w http.ResponseWriter, r *http.Request) {
 	if chamber == 0 {
 		w.WriteHeader(http.StatusTeapot)
 		w.Write([]byte("you die!"))
+
+		requestsTotal.WithLabelValues("roulette", fmt.Sprint(http.StatusTeapot)).Inc()
+
 		return
 	}
 
+	requestsTotal.WithLabelValues("roulette", fmt.Sprint(http.StatusOK)).Inc()
 	w.Write([]byte("you live!"))
 }
 
